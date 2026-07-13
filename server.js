0855a8ebc09c802437b83f179e426cd5dff63ec2
kaifhassan9 <mdkaif13551@gmail.com>
2026-07-13 21:10:51 +0530

    modified server.js

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
 
-app.use(cors());          // 1. CORS first
-app.use(express.json());  // 2. JSON parser
+// Serve frontend files
+app.use(express.static(path.join(__dirname, "frontend")));
 
 // Routes
 const authRoutes = require("./routes/authRoutes");
@@ -15,7 +18,11 @@ const accountRoutes = require("./routes/accountRoutes");
 app.use("/api/auth", authRoutes);
 app.use("/api/account", accountRoutes);
 
-// Server start
+// Home route
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "login.html"));
+});
+
 app.listen(5000, () => {
-  console.log("Server running on port 5000");
+    console.log("Server running on port 5000");
 });
